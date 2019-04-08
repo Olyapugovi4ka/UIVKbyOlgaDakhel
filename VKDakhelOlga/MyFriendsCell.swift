1e2f44f4ab89ceafbaf54f302c948736fc8dc779
Olga Dakhel <olyapugovi4ka@gmail.com>
2019-04-08 23:12:01 +0300

    Добавила ячейку в друзей

---
+++ VKDakhelOlga/MyFriendsCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyFriendsCell.swift
+//  VKDakhelOlga
+//
+//  Created by MacBook on 08/04/2019.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class MyFriendsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
